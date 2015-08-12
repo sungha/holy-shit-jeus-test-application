9cf31cf5b5d0498b4c70499fd63495274ee6a01e
Sungha Jun <sh@steelheart.kr>
2015-08-12 14:38:38 +0900

    first working version. run on legacy mode. sigh..

--- src/main/java/kr/steelheart/Application.java
+++ src/main/java/kr/steelheart/Application.java
@@ -2,22 +2,19 @@ package kr.steelheart;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
-@SpringBootApplication
-public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
 
 }
