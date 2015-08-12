c5f63a3c68090b756cdf6c673e7cf4e63ba1b096
Sungha Jun <sh@steelheart.kr>
2015-08-12 14:05:51 +0900

    .

---
+++ src/main/java/kr/steelheart/Application.java
@@ -0,0 +1,23 @@
+package kr.steelheart;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+
+}
